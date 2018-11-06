1d41039a935b2f53607ff7721d0266f0d2fe833e
Stephan-Kashkarov <KashkarovWork@gmail.com>
2018-11-06 19:56:43 +1100

    added calcitemPoses function to button array

--- app/assets/text.py
+++ app/assets/text.py
@@ -3,11 +3,11 @@ import pygame as pg
 class textBox(pg.sprite.Sprite):
 	def __init__(self, title, x, y, fontSize, color, screen):
 		self.screen = screen
+		self.width, self.height = self.screen.get_rect().size
 		self.font = pg.font.SysFont("Times New Roman", fontSize)
 		self.pos = (x, y)
 		self.fontSize = fontSize
 		self.textSurf = self.font.render(title, False, color)
-		self.width, self.height = self.textSurf.get_rect().size
 		self.pos = (self.pos[0] - self.width/2, self.pos[1] - self.height/2)
 
 	def draw(self):
@@ -15,14 +15,27 @@ class textBox(pg.sprite.Sprite):
 
 
 class buttonArray(pg.sprite.Group):
-	def __init__(self, screen, items, vert=False):
+	def __init__(self, screen, items):
 		self.screen = screen
+		self.width, self.height = self.screen.get_rect().size
 		self.items = items
-		self.vert = vert
 		poses = self.calcitemPoses()
 		self.items = self.genButtons(poses)
 
 
 	def calcitemPoses(self):
+		poses = [x for x in self.items]
+		if len(self.items) % 2 == 0:
+			itemsLen = len(self.items)
+			mod = 0
+			for i in range(0, itemsLen/2, -1):
+				poses.insert(i, self.height + mod)
+				mod += 200
+			mod = 0
+			for i in range(itemsLen/2, itemsLen):
+				poses.insert(i, self.height - mod)
+				mod += 200
+		return poses
+
+	def genButtons(self):
 		pass
-	
